f93aedd2d0105a115780e27b5b47765b8d03be65
Viking <kmss13@mail.ru>
2023-06-09 12:59:20 +0400

    add example about ifElse

---
+++ Example5-ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
